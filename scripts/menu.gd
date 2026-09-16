90deb0c3182c689616d1bf405dc8eff0a9893ec3
masterofmagic234 <masterofmagic234@gmail.com>
2026-09-16 21:24:49 +0400

    Start game through opening cutscene

--- scripts/menu.gd
+++ scripts/menu.gd
@@ -4,4 +4,4 @@ func _ready() -> void:
     $StartButton.grab_focus()
 
 func _on_start_pressed() -> void:
-    get_tree().change_scene_to_file("res://game.tscn")
+    get_tree().change_scene_to_file("res://intro_cutscene.tscn")
