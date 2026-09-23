abf526bafc40e0311ee040942163f34948046d45
masterofmagic234 <masterofmagic234@gmail.com>
2026-09-23 21:32:38 +0300

    test: load newly rigged animated Scout model

--- tools/level1_smoke_test.gd
+++ tools/level1_smoke_test.gd
@@ -76,7 +76,7 @@ func _run() -> void:
         _fail("Level 1 lanterns missing")
         return
 
-    var squirrel_model := load("res://cartoon+squirrel+3d+model.glb") as PackedScene
+    var squirrel_model := load("res://Meshy_AI_Acorn_Guardian_0923182156_texture (1).glb") as PackedScene
     if squirrel_model == null:
         _fail("3D Scout model could not be loaded")
         return
