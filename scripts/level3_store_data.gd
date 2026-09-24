fd51265528253f4e23768abe9bfbc1fea19aa032
masterofmagic234 <masterofmagic234@gmail.com>
2026-09-25 00:33:08 +0300

    Remove resource-instance constant dependency in Level 3 map data

--- scripts/level3_store_data.gd
+++ scripts/level3_store_data.gd
@@ -166,8 +166,8 @@ static func get_door_cells() -> Array[Vector2i]:
             if MAP_DATA.tile_at(Vector2i(x, y)) != 2:
                 continue
 
-            var has_left_pair := x > 0 and MAP_DATA.tile_at(Vector2i(x - 1, y)) == MAP_DATA.DOOR
-            var has_up_pair := y > 0 and MAP_DATA.tile_at(Vector2i(x, y - 1)) == MAP_DATA.DOOR
+            var has_left_pair := x > 0 and MAP_DATA.tile_at(Vector2i(x - 1, y)) == 2
+            var has_up_pair := y > 0 and MAP_DATA.tile_at(Vector2i(x, y - 1)) == 2
             if has_left_pair or has_up_pair:
                 continue
 
